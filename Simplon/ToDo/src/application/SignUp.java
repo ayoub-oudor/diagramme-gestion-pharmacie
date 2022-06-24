3cdc2f1b0461413920e997f84109d4cb70e5a0e2
AYOUB OUDOR <56844344+ayoub-oudor@users.noreply.github.com>
2022-06-24 18:01:24 +0200

    Delete Simplon/ToDo directory

--- Simplon/ToDo/src/application/SignUp.java
+++
@@ -1,9 +0,0 @@
-package application;
-
-
- 
-public class SignUp {
-	public static void main(String[] args) {
-
-	}
-}
--- Simplon/ToDo/src/dao/interface_verification.java
+++
@@ -1,7 +0,0 @@
-package dao;
-
-public interface interface_verification {
-	
-	boolean login(String username, String password);
-	
-}
